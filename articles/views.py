d2b4412a67921c042787a44f33626bca28b8741c
Adele Alibeigi <adelealibeigi@gmail.com>
2021-07-26 18:39:56 +0430

    Permissions and Authorization

--- articles/views.py
+++ articles/views.py
@@ -1,34 +1,65 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import (UpdateView, DeleteView, CreateView)
 from django.urls import reverse_lazy
 from .models import Article
-from django.views.generic.edit import (UpdateView, DeleteView, CreateView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
-class ArticleCreateView(CreateView):
+"""
+    use LoginRequiredMixin left of CreateView, To restrict view access to only logged in users,
+    We want the CreateView to already know we intend to restrict access.
+    we avoid resubmitting the form and access when user not log in!
+    Django has automatically redirected users to the log in page.
+"""
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields = ('title', 'body', 'author',)
+    fields = ('title', 'body')
+
+    # to set the current user to author we need to customize it.
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
 
 
-class ArticleUpdateView(UpdateView):
+"""
+    use UserPassesTestMixin => add it second in our list of mixins for BlogUpdateView.
+    That means a user must first be logged in and then they must pass the user test before accessing UpdateView
+    
+    obj.author == self.request.user => if the author on the current object matches the current user on the webpage
+    (whoever is logged in and trying to make the change), then allow it.If false, 403 error will automatically be thrown.
+    
+"""
+
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('account/', TemplateView.as_view(template_name='home.html'),name='home'),
+    path('home/', TemplateView.as_view(template_name='home.html'),name='home'),
     path('articles/', include('articles.urls')),
     path('', include('pages.urls')),
 ]
